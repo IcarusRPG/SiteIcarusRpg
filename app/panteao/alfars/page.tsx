0603c9cfd7d07d22287e7fd9d932270344edbbdb
IcarusRPG <adam.paulo@gmail.com>
2026-05-06 14:58:04 -0300

    Cria base inicial do site Icarus RPG

---
+++ app/panteao/alfars/page.tsx
@@ -0,0 +1,5 @@
+import PlaceholderPage from "@/components/sections/PlaceholderPage";
+
+export default function AlfarsPage() {
+  return <PlaceholderPage title="Alfars" eyebrow="Panteão de Icarus" />;
+}
---
+++ app/sobre-icarus/page.tsx
@@ -0,0 +1,5 @@
+import PlaceholderPage from "@/components/sections/PlaceholderPage";
+
+export default function SobreIcarusPage() {
+  return <PlaceholderPage title="Sobre Icarus" />;
+}
